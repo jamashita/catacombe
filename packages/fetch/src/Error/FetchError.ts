47e60bdb20fb3335235a54ec202d2973486d7221
jamashita <jamashita@arachnid.xyz>
2021-04-24 02:47:38 +0900

    fix: 🐛 typo

--- packages/fetch/src/Error/FetchError.ts
+++ packages/fetch/src/Error/FetchError.ts
@@ -2,7 +2,6 @@ import { DataSourceError } from '@jamashita/anden-error';
 
 export class FetchError extends DataSourceError<'FetchError', 'Fetch'> {
   public constructor(message: string, cause?: Error) {
-    å;
     super('FetchError', 'Fetch', message, cause);
   }
 }
