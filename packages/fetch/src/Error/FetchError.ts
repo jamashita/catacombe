ef602b94d2b336c3197ada78bbf3024bc8cbdef2
jamashita <jamashita@arachnid.xyz>
2021-04-24 02:46:43 +0900

    fix: 🐛 reverted

--- packages/fetch/src/Error/FetchError.ts
+++ packages/fetch/src/Error/FetchError.ts
@@ -1,14 +1,8 @@
 import { DataSourceError } from '@jamashita/anden-error';
 
 export class FetchError extends DataSourceError<'FetchError', 'Fetch'> {
-  private status: number;
-
-  public constructor(message: string, status: number, cause?: Error) {
+  public constructor(message: string, cause?: Error) {
+    å;
     super('FetchError', 'Fetch', message, cause);
-    this.status = status;
-  }
-
-  public getStatus(): number {
-    return this.status;
   }
 }
