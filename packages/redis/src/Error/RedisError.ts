abb99e7eb451d781bd023ed497aa0c3369d8e590
jamashita <jamashita@arachnid.xyz>
2021-04-29 11:03:03 +0900

    feat: 🎸 removed all DataSourceError

--- packages/redis/src/Error/RedisError.ts
+++ packages/redis/src/Error/RedisError.ts
@@ -1,7 +1,9 @@
-import { DataSourceError } from '@jamashita/anden-error';
+import { RuntimeError } from '@jamashita/anden-error';
 
-export class RedisError extends DataSourceError<'RedisError', 'Redis'> {
+export class RedisError extends RuntimeError<'RedisError'> {
+  public readonly noun: 'RedisError' = 'RedisError';
+
   public constructor(message: string, cause?: Error) {
-    super('RedisError', 'Redis', message, cause);
+    super(message, cause);
   }
 }
