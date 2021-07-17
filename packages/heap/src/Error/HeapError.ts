6bcec3be9ec84cddd81d88da52e414f012c9769a
jamashita <jamashita@arachnid.xyz>
2021-07-17 23:36:54 +0900

    refactor: 💡 override option

--- packages/heap/src/Error/HeapError.ts
+++ packages/heap/src/Error/HeapError.ts
@@ -1,7 +1,7 @@
 import { DataSourceError } from '@jamashita/catacombe-datasource';
 
 export class HeapError extends DataSourceError<'HeapError'> {
-  public readonly noun: 'HeapError' = 'HeapError';
+  public override readonly noun: 'HeapError' = 'HeapError';
 
   public constructor(message: string, cause?: Error) {
     super('HeapError', message, cause);
