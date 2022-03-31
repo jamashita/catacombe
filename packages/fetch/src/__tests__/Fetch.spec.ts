135a91f6b98bef01917b767b721c4833bb8df81d
jamashita <yamashita@arachnid.xyz>
2022-03-31 11:09:38 +0900

    test: 💍 test ordering

--- packages/fetch/src/__tests__/Fetch.spec.ts
+++ packages/fetch/src/__tests__/Fetch.spec.ts
@@ -22,6 +22,134 @@ describe('Fetch', () => {
     fetchMock.resetMocks();
   });
 
+  describe('delete', () => {
+    it('responds CONTINUE', async () => {
+      expect.assertions(1);
+
+      fetchMock.mockResponse((): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.CONTINUE,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      await expect(fetch.delete(url)).rejects.toThrow(FetchError);
+    });
+
+    it('responds OK: response is text', async () => {
+      expect.assertions(2);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.OK,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: sr
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      const r: FetchResponse<'text'> = await fetch.delete(url);
+
+      expect(r.status).toBe(StatusCodes.OK);
+      expect(r.body).toBe(sr);
+    });
+
+    it('responds OK: response is json', async () => {
+      expect.assertions(2);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.OK,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'json'> = new Fetch<'json'>('json');
+
+      const r: FetchResponse<'json'> = await fetch.delete(url);
+
+      expect(r.status).toBe(StatusCodes.OK);
+      expect(r.body).toStrictEqual(jr);
+    });
+
+    it('responds MULTIPLE_CHOICE', async () => {
+      expect.assertions(1);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.MULTIPLE_CHOICES,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      await expect(fetch.delete(url)).rejects.toThrow(FetchError);
+    });
+
+    it('responds BAD_REQUEST', async () => {
+      expect.assertions(1);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.BAD_REQUEST,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      await expect(fetch.delete(url)).rejects.toThrow(FetchError);
+    });
+
+    it('responds INTERNAL_SERVER_ERROR', async () => {
+      expect.assertions(1);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.INTERNAL_SERVER_ERROR,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      await expect(fetch.delete(url)).rejects.toThrow(FetchError);
+    });
+  });
+
   describe('get', () => {
     it('responds CONTINUE', async () => {
       expect.assertions(1);
@@ -147,6 +275,134 @@ describe('Fetch', () => {
       const fetch: Fetch<'text'> = new Fetch('text');
 
       await expect(fetch.get(url)).rejects.toThrow(FetchError);
+    });
+  });
+
+  describe('head', () => {
+    it('responds CONTINUE', async () => {
+      expect.assertions(1);
+
+      fetchMock.mockResponse((): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.CONTINUE,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      await expect(fetch.head(url)).rejects.toThrow(FetchError);
+    });
+
+    it('responds OK: response is text', async () => {
+      expect.assertions(2);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.OK,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: sr
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      const r: FetchResponse<'text'> = await fetch.head(url);
+
+      expect(r.status).toBe(StatusCodes.OK);
+      expect(r.body).toBe(sr);
+    });
+
+    it('responds OK: response is json', async () => {
+      expect.assertions(2);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.OK,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'json'> = new Fetch<'json'>('json');
+
+      const r: FetchResponse<'json'> = await fetch.head(url);
+
+      expect(r.status).toBe(StatusCodes.OK);
+      expect(r.body).toStrictEqual(jr);
+    });
+
+    it('responds MULTIPLE_CHOICE', async () => {
+      expect.assertions(1);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.MULTIPLE_CHOICES,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      await expect(fetch.head(url)).rejects.toThrow(FetchError);
+    });
+
+    it('responds BAD_REQUEST', async () => {
+      expect.assertions(1);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.BAD_REQUEST,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      await expect(fetch.head(url)).rejects.toThrow(FetchError);
+    });
+
+    it('responds INTERNAL_SERVER_ERROR', async () => {
+      expect.assertions(1);
+
+      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
+        return Promise.resolve<MockResponseInit>({
+          init: {
+            status: StatusCodes.INTERNAL_SERVER_ERROR,
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          },
+          body: JSON.stringify(jr)
+        });
+      });
+
+      const fetch: Fetch<'text'> = new Fetch('text');
+
+      await expect(fetch.head(url)).rejects.toThrow(FetchError);
     });
   });
 
@@ -403,262 +659,6 @@ describe('Fetch', () => {
       const fetch: Fetch<'text'> = new Fetch('text');
 
       await expect(fetch.put(url)).rejects.toThrow(FetchError);
-    });
-  });
-
-  describe('delete', () => {
-    it('responds CONTINUE', async () => {
-      expect.assertions(1);
-
-      fetchMock.mockResponse((): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.CONTINUE,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      await expect(fetch.delete(url)).rejects.toThrow(FetchError);
-    });
-
-    it('responds OK: response is text', async () => {
-      expect.assertions(2);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.OK,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: sr
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      const r: FetchResponse<'text'> = await fetch.delete(url);
-
-      expect(r.status).toBe(StatusCodes.OK);
-      expect(r.body).toBe(sr);
-    });
-
-    it('responds OK: response is json', async () => {
-      expect.assertions(2);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.OK,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'json'> = new Fetch<'json'>('json');
-
-      const r: FetchResponse<'json'> = await fetch.delete(url);
-
-      expect(r.status).toBe(StatusCodes.OK);
-      expect(r.body).toStrictEqual(jr);
-    });
-
-    it('responds MULTIPLE_CHOICE', async () => {
-      expect.assertions(1);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.MULTIPLE_CHOICES,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      await expect(fetch.delete(url)).rejects.toThrow(FetchError);
-    });
-
-    it('responds BAD_REQUEST', async () => {
-      expect.assertions(1);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.BAD_REQUEST,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      await expect(fetch.delete(url)).rejects.toThrow(FetchError);
-    });
-
-    it('responds INTERNAL_SERVER_ERROR', async () => {
-      expect.assertions(1);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.INTERNAL_SERVER_ERROR,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      await expect(fetch.delete(url)).rejects.toThrow(FetchError);
-    });
-  });
-
-  describe('head', () => {
-    it('responds CONTINUE', async () => {
-      expect.assertions(1);
-
-      fetchMock.mockResponse((): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.CONTINUE,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      await expect(fetch.head(url)).rejects.toThrow(FetchError);
-    });
-
-    it('responds OK: response is text', async () => {
-      expect.assertions(2);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.OK,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: sr
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      const r: FetchResponse<'text'> = await fetch.head(url);
-
-      expect(r.status).toBe(StatusCodes.OK);
-      expect(r.body).toBe(sr);
-    });
-
-    it('responds OK: response is json', async () => {
-      expect.assertions(2);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.OK,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'json'> = new Fetch<'json'>('json');
-
-      const r: FetchResponse<'json'> = await fetch.head(url);
-
-      expect(r.status).toBe(StatusCodes.OK);
-      expect(r.body).toStrictEqual(jr);
-    });
-
-    it('responds MULTIPLE_CHOICE', async () => {
-      expect.assertions(1);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.MULTIPLE_CHOICES,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      await expect(fetch.head(url)).rejects.toThrow(FetchError);
-    });
-
-    it('responds BAD_REQUEST', async () => {
-      expect.assertions(1);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.BAD_REQUEST,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      await expect(fetch.head(url)).rejects.toThrow(FetchError);
-    });
-
-    it('responds INTERNAL_SERVER_ERROR', async () => {
-      expect.assertions(1);
-
-      fetchMock.mockResponse(async (): Promise<MockResponseInit> => {
-        return Promise.resolve<MockResponseInit>({
-          init: {
-            status: StatusCodes.INTERNAL_SERVER_ERROR,
-            headers: {
-              'Content-Type': 'text/html'
-            }
-          },
-          body: JSON.stringify(jr)
-        });
-      });
-
-      const fetch: Fetch<'text'> = new Fetch('text');
-
-      await expect(fetch.head(url)).rejects.toThrow(FetchError);
     });
   });
 });
